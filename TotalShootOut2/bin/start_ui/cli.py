54153e038434b95caa16d20743ba306e0e73f317
zannabianca1997 <zannabianca199712@gmail.com>
2016-10-07 23:09:08 +0200

    Added start screen json and procedure to Cli

--- TotalShootOut2/bin/start_ui/cli.py
+++ TotalShootOut2/bin/start_ui/cli.py
@@ -1,5 +1,34 @@
 from .start_user_interface import StartUi
 
+import shutil
+import json
+import time
+
 
 class Cli(StartUi):
-    pass
+
+    def __init__(self, default_terminal_size=(80, 20)):
+        "setup the Cli"
+        self.default_terminal_size = default_terminal_size
+
+    # StartUi implementation
+
+    def _start_screen(self):
+        "open a start screen"
+        size = self._get_terminal_size()
+        # todo: change with a paths class
+        data = json.load(open("assets/json/startup/start_screen.json"))
+        # Printing
+        print(data["content"]["title"].upper().center(size.columns))
+        print()
+        for line in data["content"]["subtitles"]:
+            print(line.center(size.columns))
+        # todo: print authors from package file
+        time.sleep(data["time"])
+
+
+    # utility
+
+    def _get_terminal_size(self):
+        "get the terminal size"
+        return shutil.get_terminal_size(self.default_terminal_size)
