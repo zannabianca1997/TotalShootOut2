e5d79a2a22208e54b8b4d8693998b26c83cf3da1
zannabianca1997 <zannabianca199712@gmail.com>
2016-10-14 22:41:12 +0200

    added docstrings

--- TotalShootOut2/bin/setups.py
+++ TotalShootOut2/bin/setups.py
@@ -5,6 +5,7 @@ import os
 import json as js
 
 class _BunchDict(dict):
+    """This dict can be called with both ["item"] and .item notation"""
     def __init__(self,kw):
         super().__init__()
         for (key, value) in kw.items():
@@ -19,11 +20,12 @@ class _BunchDict(dict):
             self[key] = value
 
     def recursive_bunching(self, value):
+        "Transform a dict of list of dicts... in a Bunch"
         if issubclass(type(value), dict):
             value = _BunchDict(value)  # itero la transformazione
         if issubclass(type(value), list):
             for i, val in enumerate(value):
-                value[i] = self.recursive_bunching(val)
+                value[i] = self.recursive_bunching(val) #se è un oggetto normale non gli farà niente
         return value
 
 
@@ -31,6 +33,7 @@ json = None
 paths = None
 
 def load_all(prj_path):
+    "Load all setups. Receive the project path as argument"
     logger.info("Loading setups...")
     #create paths dictionary
     _mount_paths(prj_path)
@@ -39,6 +42,8 @@ def load_all(prj_path):
 
 
 def _mount_paths(prj_path):
+    "Create paths dict"
+    #todo:this should be read from file
     logger.info("Loading paths...")
     bin_path = os.path.join(prj_path, "bin")
     ass_path = os.path.join(prj_path, "assets")
@@ -54,6 +59,7 @@ def _mount_paths(prj_path):
     logger.debug("Setted paths:\n   %s" , ", \n    ".join(str(paths).split(", ")))
 
 def _load_jsons():
+    "loads all files from json directory"
     logger.info("Loading jsons...")
     global json
     json = _load_dir(paths["json"]) #loading the path
@@ -61,6 +67,7 @@ def _load_jsons():
 
 
 def _load_dir(dir):
+    "load all file and subdir in the memory"
     result = dict()
     logger.debug("Navigating %s...", dir)
     for name in os.listdir(dir):
@@ -72,6 +79,7 @@ def _load_dir(dir):
     return _BunchDict(result)
 
 def _load_file(file):
+    "load a file in the memory"
     logger.debug("Loading %s", file)
     try:
         with open(file, "r") as f:
